6cff79209a07a8de55e7661a8911ff3f95ebc496
Saja-Albinali <sajaalbinali176@gmail.com>
2025-01-22 12:34:59 +0300

    setup

---
+++ capstone-project-2025-web/src/actions/config.js
@@ -0,0 +1,14 @@
+import { getToken } from "./token";
+
+const baseUrl = "http://localhost:8080";
+
+async function getHeaders({ auth = true, contentType = true } = {}) {
+  const token = await getToken();
+  const headers = new Headers();
+  if (contentType) headers.append("Content-Type", "application/json");
+  if (auth) headers.append("Authorization", `Bearer ${token}`);
+
+  return headers;
+}
+
+export { baseUrl, getHeaders };
