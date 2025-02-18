9a011971ee8c9340091c1ad07c4116140a18dbaa
Abdulrahman <Abdulrahman-g-alfahad@gmail.com>
2025-02-18 10:41:00 +0300

    added enviroment variables

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,6 +1,7 @@
 import { getToken } from "./token";
 
-const baseUrl = "http://localhost:8081";
+// const baseUrl = "http://localhost:8081";
+const baseUrl = `https://${process.env.IP}.com`;
 
 async function getHeaders({ auth = true, contentType = true } = {}) {
   const token = await getToken();
